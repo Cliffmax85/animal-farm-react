7615361c03644ab61d084df370c6e5d6e58795e2
cliffmax85 <cliffmax85@gmail.com>
2022-01-25 17:31:18 -0800

    got background working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { animals } from './data.js';
+import Header from './Header/Header.js';
+import Main from './Main/Main.js';
+import Footer from './Footer/Footer.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <Header name="Joey Joe Jo Shabadoo" />
+        <Main animals = {{ animals }} />
+        <Footer email="animal@farm.com" />
+      </div>
     </div>
   );
 }
--- src/Header/Header.js
+++ src/Header/Header.js
@@ -2,5 +2,5 @@ import React from 'react';
 import './Header.css';
 
 export default function Header(props) {
-    return <header>{props.name}</header>
+  return <header>{props.name}</header>;
 }
