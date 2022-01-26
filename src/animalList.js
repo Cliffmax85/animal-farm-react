9fa85338be7b130539a122f78cdae094ea3f2ea2
cliffmax85 <cliffmax85@gmail.com>
2022-01-25 19:01:03 -0800

    added animal.js

--- src/animalList.js
+++ src/animalList.js
@@ -1,1 +1,10 @@
 import React from 'react';
+import Animal from './Animal/Animal';
+
+export default function AnimalList(props) {
+  return <div>
+    {props.animals.map((animal, i) => 
+      <Animal key={animal.name + i} name={animal.name} type={animal.type} says={animal.say} top={animal.top} left={animal.left} />
+    )}
+  </div>;
+}
